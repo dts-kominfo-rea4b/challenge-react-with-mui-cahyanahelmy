50ac8a3784d47b864433479b2c768f54657d7beb
dvlprchelmy <dvlp.cahyanahelmy@gmail.com>
2022-09-26 06:49:14 +0700

    Tugas

--- src/App.js
+++ src/App.js
@@ -1,4 +1,23 @@
-import './App.css';
+import "./App.css";
+
+import { Card, CardContent, Grid, List } from "@mui/material";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { Box } from "@mui/system";
+import { useState } from "react";
+import "./App.css";
+import Contact from "./components/Contact";
+import ContactForm from "./components/ContactForm";
+import Header from "./components/Header";
+
+import contactsJSON from "./data/contacts.json";
+const theme = createTheme({
+  palette: {
+    mode: "light",
+    primary: {
+      main: "#E0F2F1",
+    },
+  },
+});
 
 // Uncomment untuk memuat daftar kontak
 // import contactsJSON from './data/contacts.json';
@@ -10,9 +29,39 @@ const App = () => {
   // Masukkan contacts yang sudah didapat dalam JSON sebagai initial state
   // Buatlah handler untuk menambahkan kontak baru yang akan dikirim ke ContactForm
 
+  const [contacts, setContacts] = useState(contactsJSON);
+  const addContact = (data) => {
+    console.log(data);
+    setContacts([...contacts, data]);
+  };
   return (
-    <div className="App">
-    </div>
+    <ThemeProvider theme={theme}>
+      <Box
+        sx={{
+          background: "#E0F2F1",
+        }}
+      >
+        <Header />
+        <Grid container spacing={2} padding={8}>
+          <Grid item xs={12} md={6} position={"relative"}>
+            <Box position={"sticky"} top={128}>
+              <ContactForm onSubmit={addContact} />
+            </Box>
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <Card>
+              <CardContent>
+                <List sx={{ width: "100%", maxWidth: 360, color: "white" }}>
+                  {contacts?.map((contact, index) => (
+                    <Contact data={contact} key={index} />
+                  ))}
+                </List>
+              </CardContent>
+            </Card>
+          </Grid>
+        </Grid>
+      </Box>
+    </ThemeProvider>
   );
 };
 
--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -3,13 +3,75 @@
 // dan Card
 // https://mui.com/material-ui/react-card/#basic-card
 
-const ContactForm = () => {
-    // Form berisi name, phone, email, dan photo url
-    // Buatlah state newContact berupa objek sesuai dengan data yang ada
+import { Button, Card, CardActions, CardContent, TextField } from "@mui/material";
+import { useState } from "react";
 
-    return (
-        <> </>
-    );
-}
+const ContactForm = ({ onSubmit }) => {
+  // Form berisi name, phone, email, dan photo url
+  // Buatlah state newContact berupa objek sesuai dengan data yang ada
+
+  const [name, setName] = useState("");
+  const [phone, setPhone] = useState("");
+  const [email, setEmail] = useState("");
+  const [photo, setPhoto] = useState("");
+
+  const handleSubmit = () => {
+    onSubmit({
+      name,
+      phone,
+      email,
+      photo,
+    });
+    console.info('Added to list')
+  };
+
+  return (
+    <Card sx={{}}>
+      <CardContent>
+        <TextField
+          id="name"
+          label="Name"
+          variant="standard"
+          sx={{ width: "100%" }}
+          required
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <TextField
+          id="phone"
+          label="Phone"
+          variant="standard"
+          sx={{ width: "100%" }}
+          required
+          value={phone}
+          onChange={(e) => setPhone(e.target.value)}
+        />
+        <TextField
+          id="email"
+          label="Email"
+          variant="standard"
+          sx={{ width: "100%" }}
+          required
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <TextField
+          id="photo"
+          label="Photo URL"
+          variant="standard"
+          sx={{ width: "100%" }}
+          required
+          value={photo}
+          onChange={(e) => setPhoto(e.target.value)}
+        />
+      </CardContent>
+      <CardActions>
+        <Button onClick={handleSubmit} size="small">
+          Add New
+        </Button>
+      </CardActions>
+    </Card>
+  );
+};
 
 export default ContactForm;
